312247d1bfe59fc46a2b319ed2cf41b98dd7d374
GuilhermeFalcoLemos <falco.guilherme@hotmail.com>
2026-04-17 15:22:27 -0300

    Automação de Contato

---
+++ cypress/e2e/contato.cy.js
@@ -0,0 +1,61 @@
+describe("Funcionalidade Contato", () => {
+
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/index.html')
+  });
+  it("Deve preencher formulario de contato om sucesso", () => {
+    
+    cy.get('[name="name"]').type('Guilherme')
+    cy.get('[name="email"]').type('teste.estudo@teste.com')
+    cy.get('[name="subject"]').select('Suporte Técnico')
+    cy.get('[name="message"]').type('Apenas uma mensagem teste para estudo de automação')
+    cy.get('#btn-submit').click()
+    // Resultado esperado
+    cy.contains('Contato enviado com sucesso').should('exist')
+  });
+
+  it("Vaidar mensagem de erro ao enviar sem nome", () => {
+    
+    cy.get('[name="name"]').clear()
+    cy.get('[name="email"]').type('teste.estudo@teste.com')
+    cy.get('[name="subject"]').select('Suporte Técnico')
+    cy.get('[name="message"]').type('Apenas uma mensagem teste para estudo de automação')
+    cy.get('#btn-submit').click()
+    // Resultado esperado
+    cy.get('#alert-container').should('contain', 'Por favor, preencha o campo Nome')
+  });
+
+  it("Vaidar mensagem de erro ao enviar sem email", () => {
+    
+    cy.get('[name="name"]').type('Guilherme')
+    cy.get('[name="email"]').clear()
+    cy.get('[name="subject"]').select('Suporte Técnico')
+    cy.get('[name="message"]').type('Apenas uma mensagem teste para estudo de automação')
+    cy.get('#btn-submit').click()
+    // Resultado esperado
+    cy.get('#alert-container').should('contain', 'Por favor, preencha o campo E-mail')
+  });
+
+  it("Vaidar mensagem de erro ao enviar sem assunto", () => {
+    
+    cy.get('[name="name"]').type('Guilherme')
+    cy.get('[name="email"]').type('teste.estudo@teste.com')
+    // cy.get('[name="subject"]').select('Suporte Técnico')
+    cy.get('[name="message"]').type('Apenas uma mensagem teste para estudo de automação')
+    cy.get('#btn-submit').click()
+    // Resultado esperado
+    cy.get('#alert-container').should('contain', 'Por favor, selecione o Assunto')
+  });
+
+  it("Vaidar mensagem de erro ao enviar sem mensagem", () => {
+    
+    cy.get('[name="name"]').type('Guilherme')
+    cy.get('[name="email"]').type('teste.estudo@teste.com')
+    cy.get('[name="subject"]').select('Suporte Técnico')
+    cy.get('[name="message"]').clear()
+    cy.get('#btn-submit').click()
+    // Resultado esperado
+    cy.get('#alert-container').should('contain', 'Por favor, escreva sua Mensagem')
+  });
+
+});
